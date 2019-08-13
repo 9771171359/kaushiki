209d7c3969e51a9d61d3268906c7c7cfc1d688e9
Training_B4C.05.15 <Training_B4C.05.15@BDC7-D-1SJ3N62.dir.svc.accenture.com>
2019-08-13 17:05:09 +0530

    commit1

---
+++ JavaDemo/src/pack1/Calculator.java
@@ -0,0 +1,11 @@
+package pack1;
+
+public class Calculator {
+       //if variable declared inside class it is called instance or class variable
+	public static void main(String[] args) {
+		int a=10;//local variables
+		int b=20;
+		int res=a+b;
+		System.out.println("The total mark: "+res);
+	}
+}
